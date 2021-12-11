acd869b2c2299b30e3731940cebdac7948c0032e
Ирина_Колясева <irinakolyaseva95@gmail.com>
2021-12-11 13:54:38 +0300

    Добавьте файлы проекта.

---
+++ 2 zadacha/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] slovo = s.Split(new[] { " " }, StringSplitOptions.None);
+            
+                string[] slovo = s.Split(new[] { " " }, StringSplitOptions.None);
+                string sl = "";
+                int drs = 0;
+                foreach (string ds in slovo)
+                {
+                    if (ds.Length > drs)
+                    {
+                        sl = ds;
+                        drs = ds.Length;
+                    }
+                }
+                Console.WriteLine(sl);
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ lab6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s = Console.ReadLine();
+
+            if (isPalindrom(s)==true)
+            {
+                Console.WriteLine("Это предложение является полнидромом");
+            }
+            else
+            {
+                Console.WriteLine("Это предложение не является полиндромом");
+            }
+          Console.ReadLine();
+        }
+        public static bool isPalindrom(string s)
+        {
+
+            bool p = true;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            { 
+                int m = s.Length - 1 - i;
+
+                if (s[i] != s[m])
+                {
+                    p = false;
+                }
+            }
+            return p;
+        }
+
+    }
+}
+            
